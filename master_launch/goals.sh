6163b93ccfbd75f07d455a18a6e32516e6f552a9
Herry <744475118@qq.com>
2020-07-20 21:04:39 +0800

    launch

--- master_launch/goals.sh
+++ master_launch/goals.sh
@@ -8,44 +8,62 @@
 # robot number 8
 ## collision
 
-3 3 4.5 3 6 3 3 4.5 6 4.5 3 6 4.5 6 6 6
+rosrun rvo_ros set_goals_client default 3 3 4.5 3 6 3 3 4.5 6 4.5 3 6 4.5 6 6 6
 
-6 6 4.5 6 3 6 6 4.5 3 4.5 6 3 4.5 3 3 3
+rosrun rvo_ros set_goals_client default 6 6 4.5 6 3 6 6 4.5 3 4.5 6 3 4.5 3 3 3
 
 # robot number 12
 ## collision
 
-3 2 4 2 5 2 6 2 3 3 6 3 3 4 6 4 3 5 4 5 5 5 6 5
+# rosrun rvo_ros set_goals_client default 3 2 4 2 5 2 6 2 3 3 6 3 3 4 6 4 3 5 4 5 5 5 6 5
 
-3 5 4 5 5 5 6 5 6 3 3 3 6 4 3 4 3 2 4 2 5 2 6 2
+# rosrun rvo_ros set_goals_client default 3 5 4 5 5 5 6 5 6 3 3 3 6 4 3 4 3 2 4 2 5 2 6 2
+
+rosrun rvo_ros set_goals_client default 3 3 4.5 3 6 3 3 4.5 6 4.5 3 6 4.5 6 6 6 2 2 3 2 4 2 5 2
+rosrun rvo_ros set_goals_client default 6 6 4.5 6 3 6 6 4.5 3 4.5 6 3 4.5 3 3 3 2 2 3 2 4 2 5 2
 
 # initiallize
 
-2 1 3 1 4 1 5 1 6 1 7 1 2 1.5 3 1.5 4 1.5 5 1.5 6 1.5 7 1.5
+rosrun rvo_ros set_goals_client default 2 1 3 1 4 1 5 1 6 1 7 1 2 1.5 3 1.5 4 1.5 5 1.5 6 1.5 7 1.5
 
-# sustech
+# SUSTECH
 
 ## s
-rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 4 5 3 6 3 5 3 3 4 3 5 3 5 4 5 5     # S
+rosrun rvo_ros set_goals_client default 4 7.5 5 7 3 7 4 5 2.5 6 3 5 3 3 4 2.5 5 3 5.5 4 5 5 4 1     # S
 
 ## u
 
-rosrun rvo_ros set_goals_client default 5 6 5 7 3 7 3 4 3 6 3 5 3 3 4 3 5 3 5 4 5 5     # U
+rosrun rvo_ros set_goals_client default 5 6 5 7 3 7 3 4 3 6 3 5 3 3 4 2.5 5 3 5 4 5 5 4 1     # U
 
 # s
 
-rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 4 5 3 6 3 5 3 3 4 3 5 3 5 4 5 5    # S
+rosrun rvo_ros set_goals_client default 4 7.5 5 7 3 7 4 5 2.5 6 3 5 3 3 4 2.5 5 3 5.5 4 5 5 4 1    # S
 
 # t
-rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 4 5 2 7 4 6 4 3 4 4 7 2 8 2 6 7    # T
+rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 4 5 2 7 4 6 4 3 4 4 3 1 5 1 6 7 4 1   # T
 
 # e
-rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 3 4 3 6 3 5 4 3 3 3 5 3 5 5 4 5   # E
+rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 3 4 3 6 3 5 4 3 3 3 5 3 5 5 4 5 4 1  # E
 
 # c
 
-rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 3 4 3 6 3 5 4 3 3 3 5 3 7 2 8 2    # C
+rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 2.5 4 2.5 6 2 5 4 3 3 3 5 3 7 1 6 1 5 1    # C
 
 # H
 
-rosrun rvo_ros set_goals_client default 5 6 5 7 3 7 3 4 3 6 3 5 4 5 3 3 5 3 5 4 5 5   # H
+rosrun rvo_ros set_goals_client default 5 6 5 7 3 7 3 4 3 6 3 5 4 5 3 3 5 3 5 4 5 5 4 1  # H
+
+# HUA WEI
+
+rosrun rvo_ros set_goals_client default 5 6 5 7 3 7 3 4 3 6 3 5 4 5 3 3 5 3 5 4 5 5 4 1  # H
+
+rosrun rvo_ros set_goals_client default 5 6 5 7 3 7 3 4 3 6 3 5 3 3 4 2.5 5 3 5 4 5 5 4 1 # U
+
+rosrun rvo_ros set_goals_client default 4 7.5 3.5 6 4.5 6 3 5 5 5 2.5 4 3.5 4 4.5 4 5.5 4 2 3 6 3 4 1 # A
+
+rosrun rvo_ros set_goals_client default 2 6 6 6 2 5 4 5 6 5 2.5 4 3.5 4 4.5 4 5.5 4 3 3 5 3 4 1  # W
+
+rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 3 4 3 6 3 5 4 3 3 3 5 3 5 5 4 5 4 1  # E
+
+rosrun rvo_ros set_goals_client default 5 6 5 7 4 7 4 4 4 6 4 5 4 3 4 2 5 3 5 5 5 4 5 2  # I
+
