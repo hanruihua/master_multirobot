e70d8b789d5a333db5b0db64f90afb4821b7c3d9
Herry <744475118@qq.com>
2020-07-21 11:14:56 +0800

    launch

--- master_launch/goals.sh
+++ master_launch/goals.sh
@@ -67,3 +67,10 @@ rosrun rvo_ros set_goals_client default 4 7 5 7 3 7 3 4 3 6 3 5 4 3 3 3 5 3 5 5 
 
 rosrun rvo_ros set_goals_client default 5 6 5 7 4 7 4 4 4 6 4 5 4 3 4 2 5 3 5 5 5 4 5 2  # I
 
+
+
+## bag 
+
+rosrun rvo_ros set_goals_client default 3 3 4.5 3 6 3 3 4.5 6 4.5 3 6 4.5 6 6 6 2 2 3 2 4 2 4 5 
+rosrun rvo_ros set_goals_client default 6 3 3 4.5 6 4.5 3 6 4.5 6 6 6 2 2 3 2 4 2 4.5 3 3 3 4 5
+
